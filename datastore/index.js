a6dd649673eb5b41aa698c08c2527a5cc698ef02
Chuck Song <chuckthedeveloper@gmail.com>
2022-08-03 23:03:13 -0400

    Complete updating a todo

--- datastore/index.js
+++ datastore/index.js
@@ -47,13 +47,21 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  var fileName = id + '.txt';
+  var filePath = exports.dataDir + '/' + fileName;
+  fs.readFile(filePath, (err, contents) => {
+    if (err) {
+      callback (new Error(`No item with id: ${id}`));
+    } else {
+      fs.writeFile(filePath, text, (err) => {
+        if (err) {
+          throw ('error updating');
+        } else {
+          callback(null, {id, text});
+        }
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
