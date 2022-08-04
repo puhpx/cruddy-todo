6f6e2ba72fff0149dc5deb95d28ff8e0f6496360
Chuck Song <chuckthedeveloper@gmail.com>
2022-08-03 23:04:08 -0400

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -65,15 +65,15 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  var fileName = id + '.txt';
+  var filePath = exports.dataDir + '/' + fileName;
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
